323c136999bbb96b783d8b75d8111e5ad41aed01
Jesus_F8 <migueldejesusugartencool@gmail.com>
2025-02-23 23:52:01 -0600

    actualizacion

--- index.js
+++ index.js
@@ -1,59 +1,52 @@
 document.addEventListener("DOMContentLoaded", function () {
-const total=document.getElementById("display");
-const btt=document.querySelectorAll(".botones button");
+    // Primero instala math.js con: npm install mathjs
+    const total = document.getElementById("display");
+    const btt = document.querySelectorAll(".botones button");
+    btt.forEach(button => {
 
+        button.addEventListener("click", function () {
+            const buttonText = this.innerText;
+            if (!(buttonText === "C" || buttonText === "←" || buttonText === "=")) {
+                display.value += buttonText;
+            }
 
+            if (buttonText === "C") {
+                display.value = "";
+            }
+            if (buttonText === "←") {
+                display.value = display.value.slice(0, -1);
+            }
 
-btt.forEach(button =>{
-
-button.addEventListener("click", function(){
- const buttonText=this.innerText;
- if(!(buttonText==="C"|| buttonText==="←"||buttonText==="="))
- {
-    alert(buttonText)
-  display.value+=buttonText;
- }
-
-     if(buttonText==="C" )                                                                                      
-        {
-            display.value="";
-        }
-    if (buttonText==="←") {
-        display.value=display.value.slice(0, -1);     
-    }    
-
-
+            if (buttonText === "=") {
+                display.value = eval(display.value);
+            }
+        });
 
     });
 
-});
 
-let suma =( ...b) =>
-    {
-     let sumt= b.reduce((acc,na)=> acc+na,0)
-    return sumt;
+    let suma = (...b) => {
+        let sumt = b.reduce((acc, na) => acc + na, 0)
+        return sumt;
     }
 
-let resta =( ...b) =>
-    {
-        let rest= b.reduce((acc,na)=> acc-na,0)
+    let resta = (...b) => {
+        let rest = b.reduce((acc, na) => acc - na, 0)
         return rest;
     }
-    
-let m =( ...b) =>
-    {
-        let mult= b.reduce((acc,na)=> acc*na,1)
+
+    let m = (...b) => {
+        let mult = b.reduce((acc, na) => acc * na, 1)
         return mult;
     }
-    
-let division =(  ...b) =>
-    {
-        let divt= b.reduce((acc,na)=> acc/na,1)
+
+    let division = (...b) => {
+        let divt = b.reduce((acc, na) => acc / na, 1)
         return divt;
     }
- 
+
 });
 
 
 
- 
+
